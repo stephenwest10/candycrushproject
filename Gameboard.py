3d8dfd2d90ba9afd82a5d345242d869c1a3dc490
Stephen West <09wests@googlemail.com>
2019-10-03 18:24:25 +0100

    Mono check now returns a position as a dictionary

--- Gameboard.py
+++ Gameboard.py
@@ -11,20 +11,28 @@ s=x-2   # this is causing a bit of a bug with the last column - need to look int
 def Gameboard(x, y):
     board = [[random.randint(1, c) for i in range(x)] for j in range(y)]         
     pprint(board) 
-
-    for i in range(0,s):                  #note that here it does the rows first before it goes through the columns, only relevant when noticing T shape monos
+    plist = {} #this is now a dictionary
+    plist2 = {}
+    for i in range(0,s+1):                  #note that here it does the rows first before it goes through the columns, only relevant when noticing T shape monos
         for j in range(0,s):
             if board[i][j] == board[i][j+1] and board[i][j+1] == board[i][j+2]:
-                board[i][j] = 0
-                board[i][j+1] = 0
-                board[i][j+2] = 0
-    for j in range(0,s):
+                #board[i][j] = 0
+                #board[i][j+1] = 0
+                #board[i][j+2] = 0
+                plist[i] = [j, j+1, j+2]
+                
+            
+    for j in range(0,s+1):
         for i in range(0,s):
             if board[i][j] == board[i+1][j] and board[i+1][j] == board[i+2][j]:
-                board[i][j] = 0
-                board[i+1][j] = 0
-                board[i+2][j] = 0
-
+                #board[i][j] = 0
+                #board[i+1][j] = 0
+                #board[i+2][j] = 0
+                plist2[j] = [i, i+1, i+2]
+                #plist2[j] = plist2 + position2
+                
+         
+    print 'the horizontal monos are here: ', plist, 'and the vertical monos are here: ', plist2
     print "Has anything changed?"  # just printing to see if it works - will delete later
     pprint(board)
 
@@ -49,3 +57,14 @@ def Gameboard(x, y):
     # this works nicely up to this point - the next step is to work out how to test a 'move'
     # function for 
 Gameboard(x,y)
+
+
+def Monochecker():
+     for i in range(0,s):                  #note that here it does the rows first before it goes through the columns, only relevant when noticing T shape monos
+        for j in range(0,s):
+            if board[i][j] == board[i][j+1] and board[i][j+1] == board[i][j+2]:
+                return board.index(i, j)
+def locationfinder():
+    for i in range(0, x):
+        location = [idx for idx, n in enumerate(board[i]) if n == 0]
+        print "Indicies: " + str(location)
